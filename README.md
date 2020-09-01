27ea0199806c3559845eeec11833bcc41e55452d
Pavel <pavel.feldman@gmail.com>
2020-09-01 12:46:21 -0700

    feat(screenshot, pdf): add support for capturing screenshots

--- README.md
+++ README.md
@@ -8,26 +8,30 @@ $ npm install --save-dev playwright-cli
 
 ## Usage
 
+```sh
 Usage: playwright-cli [options] [command]
 
 Options:
-  -V, --version                output the version number
-  -b, --browser <browserType>  browser to use, one of cr, chromium, ff, firefox, wk, webkit (default: "chromium")
-  --color-scheme <scheme>      emulate preferred color scheme, "light" or "dark"
-  --device <deviceName>        emulate device, for example  "iPhone 11"
-  --geolocation <coordinates>  specify geolocation coordinates, for example "37.819722,-122.478611"
-  --headless                   run in headless mode (default: false)
-  --lang <language>            specify language / locale, for example "en-GB"
-  --proxy-server <proxy>       specify proxy server, for example "http://myproxy:3128" or "socks5://myproxy:8080"
-  --timezone <time zone>       time zone to emulate, for example "Europe/Rome"
-  --user-agent <ua string>     specify user agent string
-  --viewport-size <size>       specify browser viewport size in pixels, for example "1280, 720"
-  -h, --help                   display help for command
+  -V, --version                          output the version number
+  -b, --browser <browserType>            browser to use, one of cr, chromium, ff, firefox, wk, webkit (default: "chromium")
+  --color-scheme <scheme>                emulate preferred color scheme, "light" or "dark"
+  --device <deviceName>                  emulate device, for example  "iPhone 11"
+  --geolocation <coordinates>            specify geolocation coordinates, for example "37.819722,-122.478611"
+  --lang <language>                      specify language / locale, for example "en-GB"
+  --proxy-server <proxy>                 specify proxy server, for example "http://myproxy:3128" or "socks5://myproxy:8080"
+  --timezone <time zone>                 time zone to emulate, for example "Europe/Rome"
+  --timeout <timeout>                    timeout for Playwright actions in milliseconds, defaults to 10000 (default: "10000")
+  --user-agent <ua string>               specify user agent string
+  --viewport-size <size>                 specify browser viewport size in pixels, for example "1280, 720"
+  -h, --help                             display help for command
 
 Commands:
-  open [url]                   open page in browser specified via -b, --browser
-  cr [url]                     open page in Chromium
-  ff [url]                     open page in Firefox
-  wk [url]                     open page in WebKit
-  codegen [url]                open given page and generate code for user actions
-  help [command]               display help for command
+  open [url]                             open page in browser specified via -b, --browser
+  cr [url]                               open page in Chromium
+  ff [url]                               open page in Firefox
+  wk [url]                               open page in WebKit
+  codegen [url]                          open page and generate code for user actions
+  screenshot [options] <url> <filename>  capture a page screenshot
+  pdf [options] <url> <filename>         save page as pdf
+  help [command]                         display help for command
+```
--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,7 @@
 import * as program from 'commander';
 import * as playwright from 'playwright';
 import { RecorderController } from './recorder/recorderController';
-import { BrowserContext } from 'playwright';
+import { BrowserContext, Page, Browser } from 'playwright';
 
 program
     .version('Version ' + require('../package.json').version)
@@ -27,19 +27,12 @@ program
     .option('--color-scheme <scheme>', 'emulate preferred color scheme, "light" or "dark"')
     .option('--device <deviceName>', 'emulate device, for example  "iPhone 11"')
     .option('--geolocation <coordinates>', 'specify geolocation coordinates, for example "37.819722,-122.478611"')
-    .option('--headless', 'run in headless mode', false)
     .option('--lang <language>', 'specify language / locale, for example "en-GB"')
     .option('--proxy-server <proxy>', 'specify proxy server, for example "http://myproxy:3128" or "socks5://myproxy:8080"')
     .option('--timezone <time zone>', 'time zone to emulate, for example "Europe/Rome"')
+    .option('--timeout <timeout>', 'timeout for Playwright actions in milliseconds, defaults to 10000', "10000")
     .option('--user-agent <ua string>', 'specify user agent string')
     .option('--viewport-size <size>', 'specify browser viewport size in pixels, for example "1280, 720"');
-
-
-const browsers = [
-  { alias: 'cr', name: 'Chromium', type: 'chromium' },
-  { alias: 'ff', name: 'Firefox', type: 'firefox' },
-  { alias: 'wk', name: 'WebKit', type: 'webkit' },
-];
 
 program
     .command('open [url]')
@@ -53,6 +46,12 @@ program
       console.log('  $ open');
       console.log('  $ -b webkit open https://example.com');
     });
+
+const browsers = [
+  { alias: 'cr', name: 'Chromium', type: 'chromium' },
+  { alias: 'ff', name: 'Firefox', type: 'firefox' },
+  { alias: 'wk', name: 'WebKit', type: 'webkit' },
+];
 
 for (const {alias, name, type} of browsers) {
   program
@@ -70,15 +69,43 @@ for (const {alias, name, type} of browsers) {
 
 program
     .command('codegen [url]')
-    .description('open given page and generate code for user actions')
+    .description('open page and generate code for user actions')
     .action(function(url, command) {
-      record(command.parent, url);
+      codegen(command.parent, url);
     }).on('--help', function() {
       console.log('');
       console.log('Examples:');
       console.log('');
       console.log('  $ record');
       console.log('  $ -b webkit record https://example.com');
+    });
+
+program
+    .command('screenshot <url> <filename>')
+    .description('capture a page screenshot')
+    .option('--wait-for-selector <selector>', 'wait for selector before taking a screenshot')
+    .option('--wait-for-timeout <timeout>', 'wait for timeout in milliseconds before taking a screenshot')
+    .action(function(url, filename, command) {
+      screenshot(command.parent, command, url, filename);
+    }).on('--help', function() {
+      console.log('');
+      console.log('Examples:');
+      console.log('');
+      console.log('  $ -b webkit screenshot https://example.com example.png');
+    });
+
+program
+    .command('pdf <url> <filename>')
+    .description('save page as pdf')
+    .option('--wait-for-selector <selector>', 'wait for given selector before saving as pdf')
+    .option('--wait-for-timeout <timeout>', 'wait for given timeout in milliseconds before saving as pdf')
+    .action(function(url, filename, command) {
+      pdf(command.parent, command, url, filename);
+    }).on('--help', function() {
+      console.log('');
+      console.log('Examples:');
+      console.log('');
+      console.log('  $ pdf https://example.com example.png');
     });
 
 program.parse(process.argv);
@@ -91,16 +118,22 @@ type Options = {
   headless: boolean,
   lang: string,
   proxyServer: string,
-  timezone: string | undefined,
+  timeout: string | undefined,
+  timezone: string,
   viewportSize: string | undefined,
-  userAgent: string | undefined
+  userAgent: string | undefined,
 };
 
-async function launchContext(options: Options): Promise<{ browserName: string, launchOptions: playwright.LaunchOptions, contextOptions: playwright.BrowserContextOptions, context: BrowserContext }> {
+type CaptureOptions = {
+  waitForSelector: string | undefined,
+  waitForTimeout: string | undefined
+};
+
+async function launchContext(options: Options, headless: boolean): Promise<{ browser: Browser, browserName: string, launchOptions: playwright.LaunchOptions, contextOptions: playwright.BrowserContextOptions, context: BrowserContext }> {
   const browserType = lookupBrowserType(options.browser);
   validateOptions(options);
   const launchOptions: playwright.LaunchOptions = {
-    headless: options.headless,
+    headless,
   };
   const contextOptions: playwright.BrowserContextOptions = options.device ? playwright.devices[options.device] : {};
 
@@ -170,25 +203,59 @@ async function launchContext(options: Options): Promise<{ browserName: string, l
         browser.close();
     })
   });
-  return { browserName: browserType.name(), context, contextOptions, launchOptions };
+  context.setDefaultTimeout(parseInt(options.timeout, 10));
+  context.setDefaultTimeout(parseInt(options.timeout, 10));
+  return { browser, browserName: browserType.name(), context, contextOptions, launchOptions };
 }
 
-async function openPage(context: playwright.BrowserContext, url: string | undefined) {
+async function openPage(context: playwright.BrowserContext, url: string | undefined): Promise<Page> {
   const page = await context.newPage();
   if (url) {
     if (!url.startsWith('http'))
       url = 'http://' + url;
     await page.goto(url);
   }
+  return page;
 }
 
 async function open(options: Options, url: string | undefined) {
-  const { context } = await launchContext(options);
+  const { context } = await launchContext(options, false);
   await openPage(context, url);
 }
 
-async function record(options: Options, url: string | undefined) {
-  const { context, browserName, launchOptions, contextOptions } = await launchContext(options);
+async function waitForPage(page: Page, captureOptions: CaptureOptions) {
+  if (captureOptions.waitForSelector) {
+    console.log(`Waiting for selector ${captureOptions.waitForSelector}...`);
+    await page.waitForSelector(captureOptions.waitForSelector);
+  }
+  if (captureOptions.waitForTimeout) {
+    console.log(`Waiting for timeout ${captureOptions.waitForTimeout}...`);
+    await page.waitForTimeout(parseInt(captureOptions.waitForTimeout, 10));
+  }
+}
+
+async function screenshot(options: Options, captureOptions: CaptureOptions, url: string, path: string) {
+  const { browser, context } = await launchContext(options, true);
+  console.log('Navigating to ' + url);
+  const page = await openPage(context, url);
+  await waitForPage(page, captureOptions);
+  console.log('Capturing screenshot into ' + path);
+  await page.screenshot({ path })
+  await browser.close();
+}
+
+async function pdf(options: Options, captureOptions: CaptureOptions, url: string, path: string) {
+  const { browser, context } = await launchContext({ ...options, browser: 'chromium' }, true);
+  console.log('Navigating to ' + url);
+  const page = await openPage(context, url);
+  await waitForPage(page, captureOptions);
+  console.log('Saving as pdf into ' + path);
+  await page.pdf({ path });
+  await browser.close();
+}
+
+async function codegen(options: Options, url: string | undefined) {
+  const { context, browserName, launchOptions, contextOptions } = await launchContext(options, false);
   new RecorderController(browserName, launchOptions, contextOptions, context, process.stdout);
   await openPage(context, url);
 }
