1ff73172d2f7a9ccff4b53f15fb87713b3dfa096
Max Schmitt <max@schmitt.mx>
2020-09-25 13:38:36 +0200

    fix(overlay): use max z-index for highlight (#85)

--- src/injected/recorder.ts
+++ src/injected/recorder.ts
@@ -53,7 +53,7 @@ export class Recorder {
         right: 0;
         bottom: 0;
         left: 0;
-        z-index: 1000000;
+        z-index: 2147483647;
         pointer-events: none;
         display: flex;
       ">
