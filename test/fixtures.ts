fdfa3717a8898a0aa3c939c62a23ab3b3e69e0e7
Pavel Feldman <pavel.feldman@gmail.com>
2020-10-12 18:54:41 -0700

    chore: roll Playwright and Folio to latest (#112)

---
+++ test/fixtures.ts
@@ -0,0 +1,261 @@
+/**
+ * Copyright Microsoft Corporation. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as http from 'http'
+import { Writable } from 'stream';
+import * as path from 'path';
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
+import * as playwright from 'playwright';
+import { folio as baseFolio } from './playwright.fixtures';
+import { ScriptController } from '../src/scriptController';
+import { Page } from 'playwright';
+import { TerminalOutput } from '../src/codegen/outputs';
+import { JavaScriptLanguageGenerator } from '../src/codegen/languages';
+export { expect, config } from 'folio';
+
+type WorkerFixtures = {
+  browserType: playwright.BrowserType<playwright.Browser>;
+  browser: playwright.Browser;
+  httpServer: httpServer;
+};
+
+type TestFixtures = {
+  contextWrapper: { context: playwright.BrowserContext, output: WritableBuffer };
+  recorder: Recorder;
+  runCLI: (args: string[]) => CLIMock;
+};
+
+export const fixtures = baseFolio.extend<WorkerFixtures, TestFixtures>();
+
+fixtures.contextWrapper.init(async ({ browser }, runTest) => {
+  const context = await browser.newContext();
+  const outputBuffer = new WritableBuffer();
+  const output = new TerminalOutput(outputBuffer as any as Writable, 'javascript')
+  const languageGenerator = new JavaScriptLanguageGenerator()
+  new ScriptController('chromium', {}, {}, context, output, languageGenerator, true);
+  await runTest({ context, output: outputBuffer });
+  await context.close();
+});
+
+fixtures.recorder.init(async ({ contextWrapper }, runTest) => {
+  const page = await contextWrapper.context.newPage();
+  if (process.env.PWCONSOLE)
+    page.on('console', console.log);
+  await runTest(new Recorder(page, contextWrapper.output));
+  await page.close();
+});
+
+fixtures.httpServer.init(async ({testWorkerIndex}, runTest) => {
+  let handler = (req: http.IncomingMessage, res: http.ServerResponse) => res.end()
+  const port = 8907 + testWorkerIndex * 2;
+  const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse)=> handler(req, res)).listen(port);
+  await runTest({
+    setHandler: newHandler => handler = newHandler,
+    PREFIX: `http://127.0.0.1:${port}`,
+  })
+  server.close()
+}, { scope: 'worker' });
+
+
+fixtures.page.override(async ({ recorder }, runTest) => {
+  await runTest(recorder.page);
+});
+
+function removeAnsiColors(input: string): string {
+  const pattern = [
+    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+  ].join('|');
+  return input.replace(new RegExp(pattern, 'g'), '');
+}
+
+class WritableBuffer {
+  _data: string;
+  private _callback: () => void;
+  _text: string;
+
+  constructor() {
+    this._data = '';
+  }
+
+  write(chunk: string) {
+    if (!chunk)
+      return;
+    if (chunk === '\u001B[F\u001B[2K') {
+      const index = this._data.lastIndexOf('\n');
+      this._data = this._data.substring(0, index);
+      return;
+    }
+    this._data += chunk;
+    if (this._callback && chunk.includes(this._text))
+      this._callback();
+  }
+
+  _waitFor(text: string): Promise<void> {
+    if (this._data.includes(text))
+      return Promise.resolve();
+    this._text = text;
+    return new Promise(f => this._callback = f);
+  }
+
+  data() {
+    return this._data;
+  }
+
+  text() {
+    return removeAnsiColors(this.data())
+  }
+}
+
+class Recorder {
+  page: playwright.Page;
+  _output: WritableBuffer;
+  _highlightCallback: Function
+  _highlightInstalled: boolean
+  _actionReporterInstalled: boolean
+  _actionPerformedCallback: Function
+
+  constructor(page: playwright.Page, output: WritableBuffer) {
+    this.page = page;
+    this._output = output;
+    this._highlightCallback = () => { };
+    this._highlightInstalled = false;
+    this._actionReporterInstalled = false;
+    this._actionPerformedCallback = () => { };
+  }
+
+  async setContentAndWait(content: string, url: string = 'about:blank') {
+    await this.setPageContentAndWait(this.page, content, url);
+  }
+
+  async setPageContentAndWait(page: Page, content: string, url: string = 'about:blank') {
+    let callback;
+    const result = new Promise(f => callback = f);
+    await page.goto(url);
+    await page.exposeBinding('_recorderScriptReadyForTest', (source, arg) => callback(arg));
+    await Promise.all([
+      result,
+      page.setContent(content)
+    ]);
+  }
+
+  async waitForOutput(text: string): Promise<void> {
+    await this._output._waitFor(text);
+  }
+
+  output(): string {
+    return this._output.text();
+  }
+
+  async waitForHighlight(action: () => Promise<void>): Promise<string> {
+    if (!this._highlightInstalled) {
+      this._highlightInstalled = true;
+      await this.page.exposeBinding('_highlightUpdatedForTest', (source, arg) => this._highlightCallback(arg));
+    }
+    const [ generatedSelector ] = await Promise.all([
+      new Promise<string>(f => this._highlightCallback = f),
+      action()
+    ]);
+    return generatedSelector;
+  }
+
+  async waitForActionPerformed(): Promise<void> {
+    if (!this._actionReporterInstalled) {
+      this._actionReporterInstalled = true;
+      await this.page.exposeBinding('_actionPerformedForTest', (source, arg) => this._actionPerformedCallback(arg));
+    }
+    await new Promise(f => this._actionPerformedCallback = f);
+  }
+
+  async hoverOverElement(selector: string): Promise<string> {
+    return this.waitForHighlight(() => this.page.dispatchEvent(selector, 'mousemove', { detail: 1 }));
+  }
+
+  async focusElement(selector: string): Promise<string> {
+    return this.waitForHighlight(() => this.page.focus(selector));
+  }
+}
+
+fixtures.runCLI.init(async ({  }, runTest) => {
+  let cli: CLIMock
+  const cliFactory = (args: string[]) => {
+    cli = new CLIMock(args);
+    return cli
+  }
+  await runTest(cliFactory);
+  cli.kill()
+});
+
+class CLIMock {
+  private process: ChildProcessWithoutNullStreams
+  private data: string;
+  private waitForText: string
+  private waitForCallback: () => void;
+  exited: Promise<number>
+
+  constructor(args: string[]) {
+    this.data = ''
+    this.process = spawn('node', [
+      path.join(__dirname, '..', 'lib', 'cli.js'),
+      ...args
+    ], {
+      env: {
+        ...process.env,
+        PWCLI_EXIT_FOR_TEST: '1'
+      }
+    });
+    this.process.stdout.on('data', line => {
+      this.data += removeAnsiColors(line.toString());
+      if (this.waitForCallback && this.data.includes(this.waitForText))
+        this.waitForCallback()
+    })
+    this.exited = new Promise(r => this.process.on('exit', r))
+  }
+
+  async waitFor(text: string): Promise<void> {
+    if (this.data.includes(text))
+      return Promise.resolve();
+    this.waitForText = text;
+    return new Promise(f => this.waitForCallback = f);
+  }
+
+  text() {
+    return removeAnsiColors(this.data);
+  }
+
+  kill() {
+    this.process.kill();
+  }
+}
+
+interface httpServer {
+  setHandler: (handler: http.RequestListener) => void
+  PREFIX: string
+}
+
+export const folio = fixtures.build();
+export const it = folio.it;
+export const fit = folio.fit;
+export const xit = folio.xit;
+export const test = folio.test;
+export const describe = folio.describe;
+export const fdescribe = folio.fdescribe;
+export const xdescribe = folio.xdescribe;
+export const beforeEach = folio.beforeEach;
+export const afterEach = folio.afterEach;
+export const beforeAll = folio.beforeAll;
+export const afterAll = folio.afterAll;
--- test/playwright.fixtures.ts
+++ test/playwright.fixtures.ts
@@ -1,7 +1,6 @@
 /**
  * Copyright Microsoft Corporation. All rights reserved.
  *
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,277 +14,192 @@
  * limitations under the License.
  */
 
-import * as http from 'http'
-import { Writable } from 'stream';
+import { config, folio as baseFolio } from 'folio';
+import type { Browser, BrowserContext, BrowserContextOptions, BrowserType, LaunchOptions, Page } from 'playwright';
 import * as path from 'path';
-import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
-import * as playwright from 'playwright';
-import { fixtures as baseFixtures } from '@playwright/test-runner';
-import { ScriptController } from '../src/scriptController';
-import { Page } from 'playwright';
-import { TerminalOutput } from '../src/codegen/outputs';
-import { JavaScriptLanguageGenerator } from '../src/codegen/languages';
+export { expect, config } from 'folio';
 
-type Parameters = {
-  browserName: string;
-}
+// Test timeout for e2e tests is 30 seconds.
+config.timeout = 30000;
 
-type WorkerFixtures = {
-  browserType: playwright.BrowserType<playwright.Browser>;
-  browser: playwright.Browser;
-  httpServer: httpServer;
+// Parameters ------------------------------------------------------------------
+// ... these can be used to run tests in different modes.
+
+type PlaywrightParameters = {
+  // Browser type name.
+  browserName: 'chromium' | 'firefox' | 'webkit';
+  // Whether to run tests headless or headful.
+  headful: boolean;
+  // Operating system.
+  platform: 'win32' | 'linux' | 'darwin';
+  // Generate screenshot on failure.
+  screenshotOnFailure: boolean;
+  // Slows down Playwright operations by the specified amount of milliseconds.
+  slowMo: number;
+  // Whether to record the execution trace.
+  trace: boolean;
 };
 
-type TestFixtures = {
-  contextWrapper: { context: playwright.BrowserContext, output: WritableBuffer };
-  page: playwright.Page;
-  recorder: Recorder;
-  runCLI: (args: string[]) => CLIMock;
+// Worker fixture declarations -------------------------------------------------
+// ... these live as long as the worker process.
+
+type PlaywrightWorkerFixtures = {
+  // Playwright library.
+  playwright: typeof import('playwright');
+  // Browser type (Chromium / WebKit / Firefox)
+  browserType: BrowserType<Browser>;
+  // Default browserType.launch() options.
+  defaultBrowserOptions: LaunchOptions;
+  // Browser instance, shared for the worker.
+  browser: Browser;
+  // True iff browserName is Chromium
+  isChromium: boolean;
+  // True iff browserName is Firefox
+  isFirefox: boolean;
+  // True iff browserName is WebKit
+  isWebKit: boolean;
+  // True iff running on Windows.
+  isWindows: boolean;
+  // True iff running on Mac.
+  isMac: boolean;
+  // True iff running on Linux.
+  isLinux: boolean;
 };
 
-export const fixtures = baseFixtures
-    .declareParameters<Parameters>()
-    .declareWorkerFixtures<WorkerFixtures>()
-    .declareTestFixtures<TestFixtures>();
 
-interface httpServer {
-  setHandler: (handler: http.RequestListener) => void
-  PREFIX: string
-}
+// Test fixture definitions, those are created for each test ------------------
 
-export function isChromium(browserName: string): boolean;
-export function isChromium(parameters: { browserName: string }): boolean;
-export function isChromium(parameters: string | { browserName: string }) {
-  return (typeof parameters === 'string' ? parameters : parameters.browserName) === 'chromium';
-}
+type PlaywrightTestFixtures = {
+  // Default browser.newContext() options.
+  defaultContextOptions: BrowserContextOptions;
+  // Factory for creating a context with given additional options.
+  contextFactory: (options?: BrowserContextOptions) => Promise<BrowserContext>;
+  // Context instance for test.
+  context: BrowserContext;
+  // Page instance for test.
+  page: Page;
+};
 
-export function isFirefox(browserName: string): boolean;
-export function isFirefox(parameters: { browserName: string }): boolean;
-export function isFirefox(parameters: string | { browserName: string }) {
-  return (typeof parameters === 'string' ? parameters : parameters.browserName) === 'firefox';
-}
+const fixtures = baseFolio.extend<PlaywrightWorkerFixtures, PlaywrightTestFixtures, PlaywrightParameters>();
+fixtures.browserName.initParameter('Browser type name', (process.env.BROWSER || 'chromium') as 'chromium' | 'firefox' | 'webkit');
+fixtures.headful.initParameter('Whether to run tests headless or headful', process.env.HEADFUL ? true : false);
+fixtures.platform.initParameter('Operating system', process.platform as ('win32' | 'linux' | 'darwin'));
+fixtures.screenshotOnFailure.initParameter('Generate screenshot on failure', false);
+fixtures.slowMo.initParameter('Slows down Playwright operations by the specified amount of milliseconds', 0);
+fixtures.trace.initParameter('Whether to record the execution trace', !!process.env.TRACING);
 
-export function isWebKit(browserName: string): boolean;
-export function isWebKit(parameters: { browserName: string }): boolean;
-export function isWebKit(parameters: string | { browserName: string }) {
-  return (typeof parameters === 'string' ? parameters : parameters.browserName) === 'webkit';
-}
-
-export function isMac() {
-  return process.platform === 'darwin';
-}
-
-fixtures.defineParameter('browserName', 'Browser type', 'chromium');
-
-fixtures.defineWorkerFixture('browserType', async ({ browserName }, test) => {
-  const browserType = playwright[browserName];
-  await test(browserType);
-});
-
-fixtures.defineWorkerFixture('browser', async ({ browserType }, test) => {
-  const browser = await browserType.launch({
-    headless: !process.env.HEADFUL
+fixtures.defaultBrowserOptions.init(async ({ headful, slowMo }, run) => {
+  await run({
+    handleSIGINT: false,
+    slowMo,
+    headless: !headful,
   });
-  await test(browser);
+}, { scope: 'worker' });
+
+fixtures.playwright.init(async ({ }, run) => {
+  const playwright = require('playwright');
+  await run(playwright);
+}, { scope: 'worker' });
+
+fixtures.browserType.init(async ({ playwright, browserName }, run) => {
+  const browserType = (playwright as any)[browserName];
+  await run(browserType);
+}, { scope: 'worker' });
+
+fixtures.browser.init(async ({ browserType, defaultBrowserOptions }, run) => {
+  const browser = await browserType.launch(defaultBrowserOptions);
+  await run(browser);
   await browser.close();
-});
+}, { scope: 'worker' });
 
-fixtures.defineWorkerFixture('httpServer', async ({parallelIndex}, runTest) => {
-  let handler = (req: http.IncomingMessage, res: http.ServerResponse) => res.end()
-  const port = 8907 + parallelIndex * 2;
-  const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse)=> handler(req, res)).listen(port);
-  await runTest({
-    setHandler: newHandler => handler = newHandler,
-    PREFIX: `http://127.0.0.1:${port}`,
-  })
-  server.close()
-})
+fixtures.isChromium.init(async ({ browserName }, run) => {
+  await run(browserName === 'chromium');
+}, { scope: 'worker' });
 
-fixtures.defineTestFixture('contextWrapper', async ({ browser }, runTest, info) => {
-  const context = await browser.newContext();
-  const outputBuffer = new WritableBuffer();
-  const output = new TerminalOutput(outputBuffer as any as Writable, 'javascript')
-  const languageGenerator = new JavaScriptLanguageGenerator()
-  new ScriptController('chromium', {}, {}, context, output, languageGenerator, true);
-  await runTest({ context, output: outputBuffer });
-  await context.close();
-});
+fixtures.isFirefox.init(async ({ browserName }, run) => {
+  await run(browserName === 'firefox');
+}, { scope: 'worker' });
 
-fixtures.defineTestFixture('recorder', async ({ contextWrapper }, runTest) => {
-  const page = await contextWrapper.context.newPage();
-  if (process.env.PWCONSOLE)
-    page.on('console', console.log);
-  await runTest(new Recorder(page, contextWrapper.output));
-  await page.close();
-});
+fixtures.isWebKit.init(async ({ browserName }, run) => {
+  await run(browserName === 'webkit');
+}, { scope: 'worker' });
 
-fixtures.defineTestFixture('page', async ({ recorder }, runTest) => {
-  await runTest(recorder.page);
-});
+fixtures.isWindows.init(async ({ platform }, run) => {
+  await run(platform === 'win32');
+}, { scope: 'worker' });
 
-function removeAnsiColors(input: string): string {
-  const pattern = [
-    '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-    '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
-  ].join('|');
-  return input.replace(new RegExp(pattern, 'g'), '');
-}
+fixtures.isMac.init(async ({ platform }, run) => {
+  await run(platform === 'darwin');
+}, { scope: 'worker' });
 
-class WritableBuffer {
-  _data: string;
-  private _callback: () => void;
-  _text: string;
+fixtures.isLinux.init(async ({ platform }, run) => {
+  await run(platform === 'linux');
+}, { scope: 'worker' });
 
-  constructor() {
-    this._data = '';
+fixtures.defaultContextOptions.init(async ({ trace, testInfo }, run) => {
+  if (trace || testInfo.retry) {
+    await run({
+      _traceResourcesPath: path.join(config.outputDir, 'trace-resources'),
+      _tracePath: testInfo.outputPath('playwright.trace'),
+      videosPath: testInfo.outputPath(''),
+    } as any);
+  } else {
+    await run({});
   }
+});
 
-  write(chunk: string) {
-    if (!chunk)
-      return;
-    if (chunk === '\u001B[F\u001B[2K') {
-      const index = this._data.lastIndexOf('\n');
-      this._data = this._data.substring(0, index);
-      return;
+fixtures.contextFactory.init(async ({ browser, defaultContextOptions, testInfo, screenshotOnFailure }, run) => {
+  const contexts: BrowserContext[] = [];
+  async function contextFactory(options: BrowserContextOptions = {}) {
+    const context = await browser.newContext({ ...defaultContextOptions, ...options });
+    contexts.push(context);
+    return context;
+  }
+  await run(contextFactory);
+
+  if (screenshotOnFailure && (testInfo.status !== testInfo.expectedStatus)) {
+    let ordinal = 0;
+    for (const context of contexts) {
+      for (const page of context.pages())
+        await page.screenshot({ timeout: 5000, path: testInfo.outputPath + `-test-failed-${++ordinal}.png` });
     }
-    this._data += chunk;
-    if (this._callback && chunk.includes(this._text))
-      this._callback();
   }
-
-  _waitFor(text: string): Promise<void> {
-    if (this._data.includes(text))
-      return Promise.resolve();
-    this._text = text;
-    return new Promise(f => this._callback = f);
-  }
-
-  data() {
-    return this._data;
-  }
-
-  text() {
-    return removeAnsiColors(this.data())
-  }
-}
-
-class Recorder {
-  page: playwright.Page;
-  _output: WritableBuffer;
-  _highlightCallback: Function
-  _highlightInstalled: boolean
-  _actionReporterInstalled: boolean
-  _actionPerformedCallback: Function
-
-  constructor(page: playwright.Page, output: WritableBuffer) {
-    this.page = page;
-    this._output = output;
-    this._highlightCallback = () => { };
-    this._highlightInstalled = false;
-    this._actionReporterInstalled = false;
-    this._actionPerformedCallback = () => { };
-  }
-
-  async setContentAndWait(content: string, url: string = 'about:blank') {
-    await this.setPageContentAndWait(this.page, content, url);
-  }
-
-  async setPageContentAndWait(page: Page, content: string, url: string = 'about:blank') {
-    let callback;
-    const result = new Promise(f => callback = f);
-    await page.goto(url);
-    await page.exposeBinding('_recorderScriptReadyForTest', (source, arg) => callback(arg));
-    await Promise.all([
-      result,
-      page.setContent(content)
-    ]);
-  }
-
-  async waitForOutput(text: string): Promise<void> {
-    await this._output._waitFor(text);
-  }
-
-  output(): string {
-    return this._output.text();
-  }
-
-  async waitForHighlight(action: () => Promise<void>): Promise<string> {
-    if (!this._highlightInstalled) {
-      this._highlightInstalled = true;
-      await this.page.exposeBinding('_highlightUpdatedForTest', (source, arg) => this._highlightCallback(arg));
-    }
-    const [ generatedSelector ] = await Promise.all([
-      new Promise<string>(f => this._highlightCallback = f),
-      action()
-    ]);
-    return generatedSelector;
-  }
-
-  async waitForActionPerformed(): Promise<void> {
-    if (!this._actionReporterInstalled) {
-      this._actionReporterInstalled = true;
-      await this.page.exposeBinding('_actionPerformedForTest', (source, arg) => this._actionPerformedCallback(arg));
-    }
-    await new Promise(f => this._actionPerformedCallback = f);
-  }
-
-  async hoverOverElement(selector: string): Promise<string> {
-    return this.waitForHighlight(() => this.page.dispatchEvent(selector, 'mousemove', { detail: 1 }));
-  }
-
-  async focusElement(selector: string): Promise<string> {
-    return this.waitForHighlight(() => this.page.focus(selector));
-  }
-}
-
-fixtures.defineTestFixture('runCLI', async ({  }, runTest, info) => {
-  let cli: CLIMock
-  const cliFactory = (args: string[]) => {
-    cli = new CLIMock(args);
-    return cli
-  }
-  await runTest(cliFactory);
-  cli.kill()
+  for (const context of contexts)
+    await context.close();
 });
 
-class CLIMock {
-  private process: ChildProcessWithoutNullStreams
-  private data: string;
-  private waitForText: string
-  private waitForCallback: () => void;
-  exited: Promise<number>
+fixtures.context.init(async ({ contextFactory }, run) => {
+  const context = await contextFactory();
+  await run(context);
+  // Context factory is taking care of closing the context,
+  // so that it could capture a screenshot on failure.
+});
 
-  constructor(args: string[]) {
-    this.data = ''
-    this.process = spawn('node', [
-      path.join(__dirname, '..', 'lib', 'cli.js'),
-      ...args
-    ], {
-      env: {
-        ...process.env,
-        PWCLI_EXIT_FOR_TEST: '1'
-      }
-    });
-    this.process.stdout.on('data', line => {
-      this.data += removeAnsiColors(line.toString());
-      if (this.waitForCallback && this.data.includes(this.waitForText))
-        this.waitForCallback()
-    })
-    this.exited = new Promise(r => this.process.on('exit', r))
-  }
+fixtures.page.init(async ({ context }, run) => {
+  // Always create page off context so that they matched.
+  await run(await context.newPage());
+  // Context fixture is taking care of closing the page.
+});
 
-  async waitFor(text: string): Promise<void> {
-    if (this.data.includes(text))
-      return Promise.resolve();
-    this.waitForText = text;
-    return new Promise(f => this.waitForCallback = f);
-  }
+fixtures.testParametersPathSegment.override(async ({ browserName, platform }, run) => {
+  await run(browserName + '-' + platform);
+});
 
-  text() {
-    return removeAnsiColors(this.data);
-  }
+export const folio = fixtures.build();
+export const it = folio.it;
+export const fit = folio.fit;
+export const xit = folio.xit;
+export const test = folio.test;
+export const describe = folio.describe;
+export const fdescribe = folio.fdescribe;
+export const xdescribe = folio.xdescribe;
+export const beforeEach = folio.beforeEach;
+export const afterEach = folio.afterEach;
+export const beforeAll = folio.beforeAll;
+export const afterAll = folio.afterAll;
 
-  kill() {
-    this.process.kill();
-  }
-}
+// If browser is not specified, we are running tests against all three browsers.
+
+folio.generateParametrizedTests(
+  'browserName',
+  process.env.BROWSER ? [process.env.BROWSER] as any : ['chromium', 'webkit', 'firefox']);
