a41f591292015c18521d26f63224e64f7e28af72
Yury Semikhatsky <yurys@chromium.org>
2020-10-27 13:15:15 -0700

    Throw if browsers.json not found

--- src/cli.ts
+++ src/cli.ts
@@ -127,7 +127,10 @@ program
     .action(function(url, filename, command) {
       let browsersJsonDir = path.dirname(process.execPath);
       if (!fs.existsSync(path.join(browsersJsonDir, 'browsers.json'))) {
-          browsersJsonDir = path.dirname(require.resolve('playwright'));
+        browsersJsonDir = path.dirname(require.resolve('playwright'));
+        if (!fs.existsSync(path.join(browsersJsonDir, 'browsers.json'))) {
+          throw new Error('Failed to find browsers.json in ' + browsersJsonDir);
+        }
       }
       require('playwright/lib/install/installer').installBrowsersWithProgressBar(browsersJsonDir);
     });
